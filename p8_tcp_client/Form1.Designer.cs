5104c969fd666a3450a641177681a0d1ee56b5d7
gtrzaska <48324148+gtrzaska@users.noreply.github.com>
2020-05-01 18:03:30 +0200

    first commit

---
+++ p8_tcp_client/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace client {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+            this.tbHostAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nUDPort = new System.Windows.Forms.NumericUpDown();
+            this.lbMessage = new System.Windows.Forms.ListBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.messageBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.send = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbHostAddress
+            // 
+            this.tbHostAddress.Location = new System.Drawing.Point(86, 20);
+            this.tbHostAddress.Name = "tbHostAddress";
+            this.tbHostAddress.Size = new System.Drawing.Size(148, 20);
+            this.tbHostAddress.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(339, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // nUDPort
+            // 
+            this.nUDPort.Location = new System.Drawing.Point(392, 20);
+            this.nUDPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.nUDPort.Name = "nUDPort";
+            this.nUDPort.Size = new System.Drawing.Size(120, 20);
+            this.nUDPort.TabIndex = 3;
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.FormattingEnabled = true;
+            this.lbMessage.Location = new System.Drawing.Point(28, 73);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(484, 199);
+            this.lbMessage.TabIndex = 4;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(233, 304);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 5;
+            this.Connect.Text = "Polacz";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(28, 278);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(390, 20);
+            this.messageBox.TabIndex = 6;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(437, 278);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 7;
+            this.send.Text = "Wyslij";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 334);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.nUDPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbHostAddress);
+            this.Name = "Form1";
+            this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.nUDPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbHostAddress;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown nUDPort;
+		private System.Windows.Forms.ListBox lbMessage;
+		private System.Windows.Forms.Button Connect;
+		private System.Windows.Forms.TextBox messageBox;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.Button send;
+	}
+}
+
---
+++ p8_tcp_client/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        private BinaryReader reader;
+        private BinaryWriter writer;
+        private bool activeCall = false;
+
+        string lastMessage = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            string host = tbHostAddress.Text;
+            int port = System.Convert.ToInt16(nUDPort.Value);
+
+            try
+            {
+                TcpClient client = new TcpClient(host, port);
+
+                NetworkStream ns = client.GetStream();
+                reader = new BinaryReader(ns);
+                writer = new BinaryWriter(ns);
+
+                writer.Write("password");
+                activeCall = true;
+
+                lbMessage.Items.Add("Nawiązano połączenie z  " + host + " na porcie: " + port);
+
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+                lbMessage.Items.Add("Nie udało się nawiązać połączenia");
+                activeCall = false;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (activeCall)
+            {
+                string message = "";
+
+                
+
+                while (message != "END" )
+                {
+                    message = reader.ReadString();
+                    if (message != lastMessage) { 
+                    lbMessage.Invoke(new MethodInvoker(delegate { lbMessage.Items.Add(message); }));
+                    lastMessage = message;}
+                }
+            }
+        }
+
+    
+        private void send_Click(object sender, EventArgs e)
+        {
+            string message = "Client: " + messageBox.Text;
+            writer.Write(message);
+            lbMessage.Invoke(new MethodInvoker(delegate { lbMessage.Items.Add(message); }));
+        }
+    }
+}
